0f844f33e4bd20f2e555e1f6714fc02fbb935258
Liang <liangr@ca.metsci.com>
2017-07-26 15:22:28 -0700

    enhanced bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -31,7 +31,7 @@ public class BubbleSort {
 		
 		int temp;
 		for (int i=0; i<array.length-1; i++){
-			for (int j=0; j<array.length-1; j++){
+			for (int j=0; j<array.length-1-i; j++){
 				if (array[j] > array[j+1]){
 					temp = array[j];
 					array[j]=array[j+1];
