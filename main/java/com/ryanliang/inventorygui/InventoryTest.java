82d7129ac2ff602672ef6cbe0e5159c459483958
Liang <liangr@ca.metsci.com>
2017-09-21 17:07:45 -0700

    sample java logging code added

--- main/java/com/ryanliang/inventorygui/InventoryTest.java
+++ main/java/com/ryanliang/inventorygui/InventoryTest.java
@@ -6,10 +6,28 @@
 package com.ryanliang.inventorygui;
 
 import java.awt.EventQueue;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class InventoryTest {
-	
+    private static Logger logger = Logger.getLogger("com.ryanliang.inventorygui.InventoryTest");
+    private static FileHandler fh;
 	public static void main(String[] args) {
+		try {
+			fh = new FileHandler("mylog.txt");
+			fh.setFormatter(new SimpleFormatter());
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		logger.addHandler(fh);
+		logger.setLevel(Level.ALL);
+		logger.finest("Inventory app starts");
 		
 		EventQueue.invokeLater(() -> {
 				InventoryModel model = new InventoryModel();
